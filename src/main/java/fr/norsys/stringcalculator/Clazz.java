2b8fdc8d33c115c8a0c3d301dcdaf09919bbf336
Marouane ELMERROUNI <melmerrouni@norsys.fr>
2024-03-10 11:19:45 +0000

    first commit

---
+++ src/main/java/fr/norsys/stringcalculator/Clazz.java
@@ -0,0 +1,10 @@
+package fr.norsys.stringcalculator;
+
+public class Clazz {
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return 1;
+    }
+}
---
+++ src/test/java/fr/norsys/stringcalculator/ClazzTest.java
@@ -0,0 +1,13 @@
+package fr.norsys.stringcalculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ClazzTest {
+    @Test
+    public void should_return_0_when_input_is_empty() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("");
+        assertEquals(0, result);
+    }
+}
