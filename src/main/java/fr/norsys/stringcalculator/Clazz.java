449a9c30928afa8f14320472471db0b3f3264b35
karimloco4@gmail.com <karimloco4@gmail.com>
2024-03-11 14:07:44 +0000

    Question 7

--- src/main/java/fr/norsys/stringcalculator/Clazz.java
+++ src/main/java/fr/norsys/stringcalculator/Clazz.java
@@ -1,10 +1,48 @@
 package fr.norsys.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Clazz {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return 1;
+
+        List<Integer> negatives = new ArrayList<>();
+
+        String[] nums;
+
+        if (numbers.startsWith("//")) {
+            String delimiter = numbers.substring(2, numbers.indexOf("\n"));
+            if(delimiter.startsWith("[")){
+                String delim = delimiter.substring(delimiter.indexOf("[")+1,delimiter.indexOf("]"));
+                nums = numbers.substring(numbers.indexOf("\n")+1).split("\\Q"+delim+"\\E");
+            }
+            else {
+                nums = numbers.substring(numbers.indexOf("\n") + 1).split(delimiter);
+            }
+        } else {
+            nums = numbers.split("[,\n]");
+        }
+
+        int sum = Arrays.stream(nums)
+                .mapToInt(Integer::parseInt)
+                .peek(num -> {
+                    if (num < 0) {
+                        negatives.add(num);
+                    }
+                })
+                .filter(num->num<=1000)
+                .sum();
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("Negative numbers arent allowed: " + negatives);
+        }
+
+        return sum;
     }
+
 }
