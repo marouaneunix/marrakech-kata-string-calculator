84a0da0c474306cdc6823d25ca44d8258c01e554
belcaid <belcaid.abdellah2001@gmail.com>
2024-03-11 16:08:10 +0100

    fix some issues and typos in add method

--- src/main/java/fr/norsys/stringcalculator/Clazz.java
+++ src/main/java/fr/norsys/stringcalculator/Clazz.java
@@ -2,33 +2,36 @@ package fr.norsys.stringcalculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.function.Predicate;
 
 public class Clazz {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
-        } else {
-            String delimiter ="[,\n]";
-            String [] numbersCopyParts= numbers.split("\n",2);
-            Arrays.stream(numbersCopyParts).forEach(System.out::println);
-            if(numbersCopyParts.length > 1 && numbersCopyParts[0].startsWith("//")){
-                delimiter  = Arrays.toString(numbersCopyParts[0].substring(2).split("[\\[\\]]"));
-                numbers=numbersCopyParts[1];
-            };
-            String[] nums = numbers.split(delimiter);
-            int sum = 0;
-            ArrayList<String> negativeNums = new ArrayList<>();
-            System.out.println(delimiter);
-            Arrays.stream(nums).forEach(System.out::println);
-
-            for (String num : nums) {
-                var intNum= !num.isEmpty() ? Integer.parseInt(num.trim()):0;
-                if(intNum<0) negativeNums.add(String.valueOf(intNum));
-                sum += intNum>1000 ?0: intNum;
-            }
-            if(!negativeNums.isEmpty()) throw new RuntimeException("negatives not allowed - "+negativeNums.toString());
-            return sum;
         }
+        String delimiter = ",\n";
+
+        if (numbers.startsWith("//")) {
+            String[] numbersCopyParts = numbers.split("\n", 2);
+            delimiter = numbersCopyParts[0].substring(2).replaceAll("[\\[\\]]", "|");
+            numbers = numbersCopyParts[1];
+        }
+        int sum = 0;
+        //split and remove empty cells if ever found
+        String[] nums = Arrays.stream(numbers.split("[" + delimiter + "]"))
+                .map(String::trim)
+                .filter(Predicate.isEqual("")
+                        .negate()).toArray(String[]::new);
+
+        ArrayList<Integer> negativeNums = new ArrayList<>();
+
+        for (String num : nums) {
+            var intNum = Integer.parseInt(num.trim());
+            if (intNum < 0) negativeNums.add(intNum);
+            sum += intNum > 1000 ? 0 : intNum;
+        }
+        if (!negativeNums.isEmpty())
+            throw new RuntimeException("negatives not allowed - " + negativeNums.toString());
+        return sum;
     }
 }
