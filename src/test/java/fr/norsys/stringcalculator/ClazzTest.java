e19f1524d8e49bbc72f5b5b7b2d618505b1e5b8e
Abdessamad <abdssamad89rahhaoui@gmail.com>
2024-03-11 09:56:18 +0000

    Q1:test case of an empty

--- src/test/java/fr/norsys/stringcalculator/ClazzTest.java
+++ src/test/java/fr/norsys/stringcalculator/ClazzTest.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test;
 public class ClazzTest {
     @Test
     public void should_return_0_when_input_is_empty() {
+        //Arrange
         Clazz clazz = new Clazz();
+        //Act
         int result = clazz.add("");
+        //Assert
         assertEquals(0, result);
     }
 
     @Test
     public void should_return_1_when_input_is_1() {
+        //Arrange
         Clazz clazz = new Clazz();
+        //Act
         int result = clazz.add("1");
+        //Assert
         assertEquals(1, result);
     }
 }
