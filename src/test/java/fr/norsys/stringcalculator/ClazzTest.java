3367812a8b324e3d688574d53841787f11e94027
omar boukioud <omarboukioudfst@gmail.com>
2024-03-11 12:11:22 +0100

    the following input is ok: “1\n2,3”

--- src/test/java/fr/norsys/stringcalculator/ClazzTest.java
+++ src/test/java/fr/norsys/stringcalculator/ClazzTest.java
@@ -38,6 +38,13 @@ public class ClazzTest {
         assertEquals(3, result);
     }
 
+    @Test
+    public void should_return_6_when_input_is_1_new_lines_2_comas_3() {
+        Clazz clazz = new Clazz();
+        int result = clazz.add("1\n2,3");
+        assertEquals(6, result);
+    }
+
 
 
 }
